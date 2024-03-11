056fc81593a18074c8c34f76011c31b08426db08
kwhproject <157696203+kwhproject@users.noreply.github.com>
2024-03-11 21:49:46 +0900

    0311

---
+++ Source/Session/Session.Build.cs
@@ -0,0 +1,20 @@
+using UnrealBuildTool;
+
+public class Session : ModuleRules
+{
+	public Session(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] 
+		{
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"HeadMountedDisplay" 
+		});
+
+		PublicIncludePaths.Add(ModuleDirectory);
+	}
+}
