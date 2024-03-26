84f5a2bbcf7394376a5de0571953ae44c30843f2
kwhproject <157696203+kwhproject@users.noreply.github.com>
2024-03-26 20:25:23 +0900

    0326

--- Source/Session/Characters/FPSCharacter.h
+++ Source/Session/Characters/FPSCharacter.h
@@ -73,4 +73,15 @@ private:
 
 	UFUNCTION(NetMulticast, Reliable)
 		void OnNetMulticast();
+
+	UFUNCTION(Client, Reliable)
+		void OnClient();
+
+	int32 RandomValue;
+
+	UPROPERTY(ReplicatedUsing = "OnRep_RandomValue")
+		int32 RandomValue_Rep;
+
+	UFUNCTION()
+		void OnRep_RandomValue();
 };
